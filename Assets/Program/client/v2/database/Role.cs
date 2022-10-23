919a735b3d9beb5fc75e4b41ef370289864b43ec
MrTiagoSan <smmdsa@gmail.com>
2022-10-22 23:52:35 -0300

    wip new dinamic way, the idea here is show how handle multiple ways to handle the same problem, and keep both methods living without problems

---
+++ Assets/Program/client/v2/database/Role.cs
@@ -0,0 +1,9 @@
+
+namespace Program.client.v2.database
+{
+    public class Role
+    {
+        public int id { get; private set; }
+        public string name { get; private set; }
+    }
+}
---
+++ Assets/Program/client/v2/database/SalaryIncrement.cs
@@ -0,0 +1,9 @@
+namespace Program.client.v2
+{
+    public class SalaryIncrement
+    {
+        public int id;
+        public string name;
+        public int porcentage;
+    }
+}
