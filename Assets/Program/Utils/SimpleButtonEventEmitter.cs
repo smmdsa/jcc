8c8cd3ac50793ad4928aedd7cd9b646f309031cc
MrTiagoSan <smmdsa@gmail.com>
2022-10-23 19:21:49 -0300

    panels and main events setup

--- Assets/Program/Utils/SimpleButtonEventEmitter.cs
+++ Assets/Program/Utils/SimpleButtonEventEmitter.cs
@@ -1,14 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using TowerDefense.ScriptableEvents;
 using UnityEngine;
-using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class SimpleButtonEventEmitter : MonoBehaviour
 {
     [SerializeField] private GameEvent _gameEvent;
-    void Awake()=>
-        this.GetComponents<Button>()[0].onClick.AddListener(()=> _gameEvent.Rise());
+    [SerializeField] private Button mButton;
+
+    private void Awake() => InitializeButton();
+    
+
+    public void InitializeButton()
+    {
+        mButton = GetComponents<Button>()[0];
+        if(mButton.onClick.GetPersistentEventCount() != 0) return;
+        mButton.onClick.AddListener(()=> _gameEvent.Rise());
+    }
     
 }
--- Assets/Program/client/v2/components/ShowEmployee.cs
+++ Assets/Program/client/v2/components/ShowEmployee.cs
@@ -1,9 +1,26 @@
+using TMPro;
 using UnityEngine;
 
 namespace Program.client.v2.components
 {
     public class ShowEmployee : MonoBehaviour
     {
-        
+        [SerializeField] private TMP_Dropdown dropdown;
+
+        private void Awake()
+        {
+            var optionDataList = new TMP_Dropdown.OptionDataList();
+            for (var i = 0; i < 10; i++)
+                optionDataList.options.Add(new TMP_Dropdown.OptionData($"hola-{i}"));
+            
+
+            dropdown.options = optionDataList.options;
+            dropdown.onValueChanged.AddListener(foo);
+        }
+
+        private void foo(int index)
+        {
+            Debug.Log($"ITEM SELECTED: {dropdown.options[index].text} ");
+        }
     }
 }
