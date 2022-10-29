6c62127b121e76c1aac86e1d1912c675e114684d
MrTiagoSan <smmdsa@gmail.com>
2022-10-29 14:20:28 -0300

    refactor for salary increment

--- Assets/Program/client/PercentEngineerSalaryIncrementer.cs
+++ Assets/Program/client/PercentEngineerSalaryIncrementer.cs
@@ -5,34 +5,6 @@ using System.Reflection.Emit;
 
 namespace Program.client
 {
-    public class SalaryIncrementer<T> : IPercentSalaryIncrementerRepository where T : Employee
-    {
-        private Dictionary<int, IPercentSalaryIncrementer> SalaryIncrementers => _salaryIncrementers ??= InitializeSalaryIncrementer();
-        private Dictionary<int, IPercentSalaryIncrementer> _salaryIncrementers;
-
-        private static Dictionary<int,IPercentSalaryIncrementer> InitializeSalaryIncrementer()
-        {
-            var output = new Dictionary<int, IPercentSalaryIncrementer>
-            {
-                { typeof(Engineer).GetHashCode(), new PercentEngineerSalaryIncrementer() },
-                { typeof(Artist).GetHashCode(), new PercentArtistSalaryIncrementer() },
-                { typeof(Designer).GetHashCode(), new PercentDesignSalaryIncrementer() },
-                { typeof(ProjectManager).GetHashCode(), new PercentProjectManagerSalaryIncrementer() },
-                { typeof(HumanResource).GetHashCode(), new PercentHumanResourceSalaryIncrementer() }
-            };
-            return output;
-        }
-
-        public IPercentSalaryIncrementer SelectSalaryIncrementer() => SalaryIncrementers[ typeof(T).GetHashCode() ];
-            
-        
-    }
-
-    public interface IPercentSalaryIncrementerRepository
-    {
-        IPercentSalaryIncrementer SelectSalaryIncrementer();
-    }
-
     public class PercentEngineerSalaryIncrementer : IPercentSalaryIncrementer
     {
         // Engineering →    (10% Seniors, 7% Semi Seniors and 5% Juniors)
